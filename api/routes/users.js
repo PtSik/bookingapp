e68869a3dfd15f604a932aea13485da852cbd23d
PtSik <patrykgniewek9@gmail.com>
2023-06-12 00:51:47 +0200

    authentication implemented

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,23 @@
 import express from "express";
+import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("Witaj użytkowniku, zostałeś zalogowany!")
+})
+
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getUsers);
 
 export default router;
