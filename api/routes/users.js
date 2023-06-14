0e551c4b27745a51aa98ccce2eca588cd4d525cd
PtSik <patrykgniewek9@gmail.com>
2023-06-14 13:45:54 +0200

    backend server finished

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,23 +1,40 @@
 import express from "express";
-import { updateUser, deleteUser, getUser, getUsers } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/checkauthentication", verifyToken, (req,res,next)=>{
-    res.send("Witaj użytkowniku, zostałeś zalogowany!")
-})
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//   res.send("Witaj użytkowniku, zostałeś zalogowany!");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send(
+//     "Witaj użytkowniku, zostałeś zalogowany i możesz usunąć swoje konto!"
+//   );
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send(
+//     "Witaj adminie, zostałeś zalogowany i możesz usunąć wszystkie konta!"
+//   );
+// });
 
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 //GET ALL
-router.get("/", getUsers);
+router.get("/",verifyAdmin , getUsers);
 
 export default router;
